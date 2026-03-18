4d6a1e524dbd1cd4cbae5999a3b4d4b3680da8b0
batangmediapartner-tech <batangmediapartner@gmail.com>
2026-03-18 04:03:36 +0000

    fix procfile

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "wms_kbr";
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 define('BASE_PATH', realpath(__DIR__ . '/../'));
 
    
